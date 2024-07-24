37d5af123313729057d854906f9bdaef91aa9aed
daehan-86 <78295295+daehan-86@users.noreply.github.com>
2024-07-24 20:08:49 +0900

    feat: abs

--- work_dir/special_functions.py
+++ work_dir/special_functions.py
@@ -2,7 +2,10 @@ def pow(a, b):
     return a**b # a^b 기능을 구현함.
 
 def abs(a):
-    pass
+    if(a<0):
+        return -a # 음수일때 양수로 전환 후 리턴
+    else:
+        return a # 양수일때 그대로 리턴
 
 def mod(a, b):
     pass
